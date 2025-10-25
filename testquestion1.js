e987d2a929f444f9a65d93e5c35b1e084d2fbb0a
Caylusis2 <caylusis2@yahoo.com>
2025-10-25 07:26:28 +0000

    Oct 25, 2025, 7:26 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let posX = 300 , xSpeed, posY = 200;
+function setup(){
+    createCanvas(600,400);
+    background('lightblue');
+    // 300 = width/2;
+    xSpeed = 5;
+    fill('limegreen');
+}
+
+function draw(){
+    background('lightblue');
+    circle(posX, posY, 50);
+    posX += xSpeed;
+    if(posX - 50/2 <=0 || posX + 50/2 >= width){
+        xSpeed = xSpeed * -1;
+    }
+    // posY += ySpeed;
+    // if(posY -50/2 <=0 || posY + 50/2 >= width){
+    // }
+}
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,11 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
+
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    yPos = 100;
+}
 
